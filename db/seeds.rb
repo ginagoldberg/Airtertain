884e50fc6548c68db7312b02fb2aa74751746886
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-08 14:47:43 +0200

    make a seed file, 4 users 2 bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.all.destroy
+
+Booking.all.destroy
+
+u1 = User.new(first_name: "Joe", last_name:"Doe", entertainer_name: "JDD")
+u2 = User.new(first_name: "Jee", last_name:"Dee", entertainer_name: "JDee")
+u3 = User.new(first_name: "Bee", last_name:"Dee", entertainer_name: "BDee")
+u4 = User.new(first_name: "Cee", last_name:"Dee")
+
+
+b1 = Booking.new(title: "Clown Party", description: "Bubbles and clowns all day", user_id: 4, entertainer_id: 1)
+b2 = Booking.new(title: "Stripper Party", description: "Bubbles and strippers all day", user_id: 2, entertainer_id: 3)
