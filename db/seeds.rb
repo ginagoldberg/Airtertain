d6f6961c3c526e5db4ccc6510a26610959fdf779
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-08 17:27:41 +0200

    add some good seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.destroy_all
 
 Booking.destroy_all
 
-u1 = User.new(first_name: "Joe", last_name:"Doe", entertainer_name: "JDD")
-u2 = User.new(first_name: "Jee", last_name:"Dee", entertainer_name: "JDee")
-u3 = User.new(first_name: "Bee", last_name:"Dee", entertainer_name: "BDee")
-u4 = User.new(first_name: "Cee", last_name:"Dee")
+u1 = User.create(first_name: "Joe", last_name:"Doe", email: "11@11.com", password: "111111", entertainer_name: "JDD")
+u2 = User.create(first_name: "Jee", last_name:"Dee", email: "21@21.com", password: "222111", entertainer_name: "JDee")
+u3 = User.create(first_name: "Bee", last_name:"Dee", email: "31@31.com", password: "333111",  entertainer_name: "BDee")
+u4 = User.create(first_name: "Cee", last_name:"Dee", email: "41@41.com", password: "444111", )
 
-b1 = Booking.new(title: "Clown Party", description: "Bubbles and clowns all day", user_id: 4, entertainer_id: 1)
-b2 = Booking.new(title: "Stripper Party", description: "Bubbles and strippers all day", user_id: 2, entertainer_id: 3)
+
+b1 = Booking.create(title: "Clown Party", description: "Bubbles and clowns all day", user_id: User.all.sample.id, entertainer_id: User.all.sample.id)
+b2 = Booking.create(title: "Stripper Party", description: "Bubbles and strippers all day", user_id: User.all.sample.id, entertainer_id: User.all.sample.id)
