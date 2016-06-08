473a8e3323bd7d2e53e069795a3f59ff21078cbb
Nadya Kalantary <nadyakalantary@gmail.com>
2016-06-08 16:22:31 +0200

    small seeds updat fix

--- app/controllers/host/bookings_controller.rb
+++ app/controllers/host/bookings_controller.rb
@@ -5,21 +5,20 @@ class Host::BookingsController < ApplicationController
   end
 
   def cancel
-
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "canceled")
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
     @booking = Booking.find(params[:id])
-    @booking.update(params.require(:booking).permit()
-      redirect_to host_path(@host)
+    @booking.update(params.require(:booking).permit(:title, :description, :start_datetime, :end_datetime, :user_id, :entertainer_id, :address_street, :address_city, :address_zip, :address_num))
+      redirect_to booking_path(@booking)
   end
-
 end
-
-
 #cancel (patch)
 #index (get)
 #edit (get)
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# User.destroy_all
-# Booking.destroy_all
-
 u1 = User.new(first_name: "Joe", last_name:"Doe", entertainer_name: "JDD")
 u2 = User.new(first_name: "Jee", last_name:"Dee", entertainer_name: "JDee")
 u3 = User.new(first_name: "Bee", last_name:"Dee", entertainer_name: "BDee")
 u4 = User.new(first_name: "Cee", last_name:"Dee")
-
 
 b1 = Booking.new(title: "Clown Party", description: "Bubbles and clowns all day", user_id: 4, entertainer_id: 1)
 b2 = Booking.new(title: "Stripper Party", description: "Bubbles and strippers all day", user_id: 2, entertainer_id: 3)
