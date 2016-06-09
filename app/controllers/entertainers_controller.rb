67f2679b850d0a0b070cf53911e487f5a2c5c91d
Gina Goldberg <gina.goldberg@hotmail.com>
2016-06-09 15:47:44 +0200

    add search bar in home page

--- app/controllers/entertainers_controller.rb
+++ app/controllers/entertainers_controller.rb
@@ -3,6 +3,15 @@ class EntertainersController < ApplicationController
     @entertainers = User.all
   end
 
+    def index
+    @entertainers = User.where("entertainer_name is NOT NULL and entertainer_name != ''")
+    if params[:search]
+      @entertainers = User.search(params[:search]).order("entertainer_name DESC")
+    else
+      @entertainers = User.all.order('entertainer_name DESC')
+    end
+  end
+
   def show
     @entertainer = User.find(params[:id])
     if @entertainer.nil?
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-layout "layoutnonav", only: :home
+  layout "layoutnonav", only: :home
   skip_before_action :authenticate_user!, only: :home
   def home
+  end
+
+  def index
+    @entertainers = User.where("entertainer_name is NOT NULL and entertainer_name != ''")
+    if params[:search]
+      @entertainers = User.search(params[:search]).order("entertainer_name DESC")
+    else
+      @entertainers = User.all.order('entertainer_name DESC')
+    end
   end
 end
